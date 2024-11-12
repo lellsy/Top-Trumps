3c9367205a7c54aebcefad33dc41547b63ac7e39
giorgia834 <106474194+giorgia834@users.noreply.github.com>
2024-11-12 22:54:53 +0000

    update

--- final.py
+++ final.py
@@ -48,7 +48,7 @@ def create_table_pokemons():
  conn.commit() #by committing we are saving our changes to the database
  conn.close() #closing the connection previously opened
 
-def insert_data_pokemons(user, round, pokemon_name, initial_hp, pokemon_id, height, weight):
+def insert_data_pokemons(user, round, pokemon_name, pokemon_id, height, weight):
  conn = sqlite3.connect('pokemons.db') #database connection
  c = conn.cursor() #cursor creates table, inserts data etc
  dictionary = {"user_name": user, "round": round,"pokemon_name": pokemon_name,"pokemon_id": pokemon_id,"height": height,"weight": weight} 
